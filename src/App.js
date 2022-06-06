dc22b6ea7446a71ca495ab05c096bcef0f580324
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-06-06 21:55:45 +0800

    Add UserForm and UserList to app interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import "./App.css";
+import { useState } from "react";
+import uuid from "react-uuid";
+import UserForm from "./components/Users/UserForm";
+import UserList from "./components/Users/UserList";
 
 function App() {
-  return "Hi";
+  const [users, setUsers] = useState([
+    {
+      id: uuid(),
+      username: "ako",
+      age: 23,
+    },
+    {
+      id: uuid(),
+      username: "siya",
+      age: 28,
+    },
+  ]);
+  const onAddUserHandler = (userInfo) => {
+    setUsers((oldUsers) => {
+      userInfo.id = uuid();
+      return [userInfo, ...oldUsers];
+    });
+  };
+
+  return (
+    <main>
+      <UserForm onAddUser={onAddUserHandler}></UserForm>
+      <UserList users={users} />
+    </main>
+  );
 }
 
 export default App;
