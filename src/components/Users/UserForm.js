c88091a8e3efd4cd2ab6d46902c9ea2c3a3d13a8
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-06-06 21:55:09 +0800

    Implement UserForm component

---
+++ src/components/Users/UserForm.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import styles from "./UserForm.module.css";
+
+export default function UserForm(props) {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("0");
+  const onUsernameChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+  const onAgeChangeHandler = (event) => {
+    setAge(event.target.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (username.trim().length === 0) {
+      return;
+    } else {
+      props.onAddUser({ username, age: Number(age) });
+      setUsername("");
+      setAge("0");
+    }
+  };
+  return (
+    <Card className={styles["user-form"]}>
+      <form onSubmit={onSubmitHandler}>
+        <section className={styles["user-form__section"]}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={onUsernameChangeHandler}
+          />
+        </section>
+        <section className={styles["user-form__section"]}>
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            value={age}
+            onChange={onAgeChangeHandler}
+            min="0"
+          />
+        </section>
+        <Button type="submit" className={styles["user-form__button"]}>
+          Add User
+        </Button>
+      </form>
+    </Card>
+  );
+}
