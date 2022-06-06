c057820f79d1f20b3fdcfb947f4bcf9053e09b78
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-06-06 21:21:44 +0800

    Implement UserItem component

--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -1,3 +1,8 @@
+import styles from "./UserItem.module.css";
+
 export default function UserItem(props) {
-  return <li>{`${props.username} (${props.age})`}</li>;
+  return (
+    <li className={styles["user-item"]}>{`${props.username} (${props.age} year${props.age > 1 ? "s" : ""
+      } old)`}</li>
+  );
 }
