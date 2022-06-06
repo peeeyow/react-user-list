b13a7ae5d560300b9ebbaa77290818973e736eda
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-06-06 21:22:08 +0800

    Implement UserList component

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,5 +1,15 @@
+import styles from "./UserList.module.css";
 import UserItem from "./UserItem";
+import Card from "../UI/Card";
 
 export default function UserList(props) {
-  return;
+  return (
+    <Card className={styles["item-list"]}>
+      <ul>
+        {props.users.map((user) => (
+          <UserItem key={user.id} username={user.username} age={user.age} />
+        ))}
+      </ul>
+    </Card>
+  );
 }
