0b6d094ed184c5effc995cbfedc7f6ee90bcf844
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-06-08 22:36:22 +0800

    Integrate ErrorModal on invalid inputs

--- src/components/Users/UserForm.js
+++ src/components/Users/UserForm.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 import styles from "./UserForm.module.css";
 
 export default function UserForm(props) {
   const [username, setUsername] = useState("");
   const [age, setAge] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [errorInfo, setErrorInfo] = useState(null);
+
   const onUsernameChangeHandler = (event) => {
     setUsername(event.target.value);
   };
@@ -14,40 +18,58 @@ export default function UserForm(props) {
   };
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    if (username.trim().length === 0) {
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      setErrorInfo({
+        title: "Invalid Input",
+        message: "Name and age must not be empty!",
+      });
+      setIsModalOpen(true);
       return;
-    } else {
-      props.onAddUser({ username, age: Number(age) });
-      setUsername("");
-      setAge("");
     }
+    if (Number(age) < 0) {
+      setErrorInfo({
+        title: "Invalid Age",
+        message: "Age must be non-negative!",
+      });
+      setIsModalOpen(true);
+      return;
+    }
+    props.onAddUser({ username, age: Number(age) });
+    setUsername("");
+    setAge("");
   };
+  const onOkayHandler = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <Card className={styles["user-form"]}>
-      <form onSubmit={onSubmitHandler}>
-        <section className={styles["user-form__section"]}>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={onUsernameChangeHandler}
-          />
-        </section>
-        <section className={styles["user-form__section"]}>
-          <label htmlFor="age">Age</label>
-          <input
-            type="number"
-            id="age"
-            value={age}
-            onChange={onAgeChangeHandler}
-            min="0"
-          />
-        </section>
-        <Button type="submit" className={styles["user-form__button"]}>
-          Add User
-        </Button>
-      </form>
-    </Card>
+    <>
+      {isModalOpen && <ErrorModal onClick={onOkayHandler} {...errorInfo} />}
+      <Card className={styles["user-form"]}>
+        <form onSubmit={onSubmitHandler}>
+          <section className={styles["user-form__section"]}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={onUsernameChangeHandler}
+            />
+          </section>
+          <section className={styles["user-form__section"]}>
+            <label htmlFor="age">Age</label>
+            <input
+              type="number"
+              id="age"
+              value={age}
+              onChange={onAgeChangeHandler}
+            />
+          </section>
+          <Button type="submit" className={styles["user-form__button"]}>
+            Add User
+          </Button>
+        </form>
+      </Card>
+    </>
   );
 }
